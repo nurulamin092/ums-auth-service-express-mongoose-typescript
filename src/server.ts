d5830f4ecdccdddffeef61e33a522390431280ba
nurulamin092 <mnurulamin092@gmial.com>
2023-05-31 20:14:03 +0600

    app.ts res change

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,19 @@
-import mongoose from "mongoose";
-import app from "./app";
-const port = 9000;
+import mongoose from 'mongoose'
+import app from './app'
+const port = 9000
 
 async function bootstrap() {
   try {
     await mongoose.connect(
       `mongodb+srv://ums-admin:HUHaUSyi6QPefW1e@cluster0.yoijtmg.mongodb.net/ums-database?retryWrites=true&w=majority`
-    );
-    console.log(`Database connect successfully...!!!!`);
+    )
+    console.log(`Database connect successfully...!!!!`)
     app.listen(port, () => {
-      console.log(`Application is listening on port ${port}`);
-    });
+      console.log(`Application is listening on port ${port}`)
+    })
   } catch (err) {
-    console.log(`Failed to connect database....`);
+    console.log(`Failed to connect database....`)
   }
 }
 
-bootstrap();
+bootstrap()
