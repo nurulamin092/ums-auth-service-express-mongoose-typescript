cf2d5476bea669eb9c3a6b0471bdd00b1c7dea9b
nurulamin092 <mnurulamin092@gmial.com>
2023-05-31 08:41:01 +0600

    project setup

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+
+const app: Application = express()
+app.use(cors())
+app.use(express.json())
+    app.use(express.urlencoded({ extended: true }))
+
+app.get('/', async (req: Request, res: Response) => {
+  res.send('Working successfully')
+})
+
+export default app
---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import app from "./app";
+const port = 9000;
+
+async function bootstrap() {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://ums-admin:HUHaUSyi6QPefW1e@cluster0.yoijtmg.mongodb.net/ums-database?retryWrites=true&w=majority`
+    );
+    console.log(`Database connect successfully...!!!!`);
+    app.listen(port, () => {
+      console.log(`Application is listening on port ${port}`);
+    });
+  } catch (err) {
+    console.log(`Failed to connect database....`);
+  }
+}
+
+bootstrap();
